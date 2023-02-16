ea3db6db473506339585a476440e1a222a53e268
PatrickOjiambo <pashrick237@gmail.com>
2023-02-16 01:08:03 -0800

    Number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
 #include<stdio.h>
 int main(void)
 	{
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
