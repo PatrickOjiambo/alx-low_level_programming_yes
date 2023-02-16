c05081b3eec4c070d85f121cd12a5c02e807a772
PatrickOjiambo <pashrick237@gmail.com>
2023-02-16 02:37:03 -0800

    Number 100

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#include<unistd.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: This program prints a message to the standard error
- *
- * Return: 1 (Error code)
+ * Return: Always 1 (Error)
  */
 int main(void)
-{
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, 59);
-    return (1);
-}
+	{
+        	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    		write(2, str, 59);
+
+    		return (1);
+	}
+
