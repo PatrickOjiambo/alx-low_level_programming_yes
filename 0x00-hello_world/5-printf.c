4571912491462ed535f9d714ceb616db4596bb2e
PatrickOjiambo <pashrick237@gmail.com>
2023-02-16 01:45:54 -0800

    Number 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-/*
+
+/**
  *Main function
+ *Main entry to the program
+ *
  */
 int main(void)
 	{
-		printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
 		return (0);
 	}
