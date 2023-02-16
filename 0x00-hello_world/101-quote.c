9a9a34237ae2748b692baa962133fe7565fd29e8
PatrickOjiambo <pashrick237@gmail.com>
2023-02-16 12:03:37 -0800

    commit 100

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,12 +9,10 @@
 int main(void)
 	{
 		
-		char *str = "and that piece of art is useful\"	- Dora Korpar,\
+	char *str = "and that piece of art is useful\" - Dora Korpar 2015-10-19\n";
 
-	        2015-10-19\n";
+	write(2, str, 59);
 
-		write(2, str, 59);
-
-		return (1);
+	return (1);
 	}
 
