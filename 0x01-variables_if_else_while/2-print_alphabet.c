11910952b9ca9ba726aadb1a207d60f4548a7fee
PatrickOjiambo <pashrick237@gmail.com>
2023-02-17 23:10:14 -0800

    Nuber 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 /*number2
-*main- Print letters of the alphabet from 
+*main- Print lowercase letters of the alphabet from 
 * 'a' to 'z'
 *
 *Return: always 0 (success)
 **/
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-        }
-        putchar('\n');
-
-        return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
 }
