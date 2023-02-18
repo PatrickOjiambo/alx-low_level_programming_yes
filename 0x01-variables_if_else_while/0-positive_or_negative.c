7d6ae1a45472541bc2a0f8a4b479c0cb04d5d822
PatrickOjiambo <pashrick237@gmail.com>
2023-02-17 19:04:16 -0800

    number 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *
- *main - Entry point
+ *main -Prints whether the value of n is postive, negative or equal to zero
  *Description: Assigns a random number to the variable n and prints if it's
  *positive, negative, or zero
  *Return: Always 0 (Success)
  */
  int main(void)
-
 	{
 	int n;
 
