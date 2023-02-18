6737c555a2198fa28cdc9ac55c7b931684d64685
PatrickOjiambo <pashrick237@gmail.com>
2023-02-17 23:26:41 -0800

    number 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *main- Print letters of the alphabet from 
+ *'a' to 'z' aprt from the letters 'q' and 'e'
+ *
+ *Return: Always zero(success)
+ *
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter!='e')||(letter!='q')
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
