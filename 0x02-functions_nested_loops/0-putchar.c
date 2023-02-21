c19269dfacf4814623ba7fd90e64779e844d73f3
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 10:01:20 -0800

    number 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/**
+ *main - Prints _putchar using _putchar function
+ *
+ *Return: Success(always true)
+ */
+#include<stdio.h>
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+
+}
