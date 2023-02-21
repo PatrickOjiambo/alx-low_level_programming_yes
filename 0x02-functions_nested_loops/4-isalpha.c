7d82927efc7a0034410b46268449f9529848d677
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 14:04:22 -0800

    Number 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isalpha - returns 1 if c is alphabet
+ *else it returns 0
+ *
+ *Return: always 0(Success)
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
