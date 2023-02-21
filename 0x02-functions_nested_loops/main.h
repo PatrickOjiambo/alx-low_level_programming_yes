067d8328fcd387abd0ae25a3a1ca10f2caee7603
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 10:09:57 -0800

    Number 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* function prototypes */
+int function1(int arg1, int arg2);
+void function2(char *str);
+int function3(void);
+
+/* prototype for _putchar */
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
