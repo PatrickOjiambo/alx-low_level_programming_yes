f0ed286db14b1768f3b2e172f9255d4f078169fa
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 11:46:23 -0800

    Num 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+/**
+ *main - Prints alphabet ten times
+ *
+ *Return: always 0 (true)
+ *
+ *
+ */
+
+#include "main"
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	int j;
+
+	for (letter = 'a';letter <= 'z';letter++)
+	{
+		_putchar(letter);
+		for(j = 0; j <= 10; j++)
+		{
+			_putchar('\n');
+		}
+	}
+}
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 /* function prototypes */
 void print_alphabet(void);
 int _putchar(char c);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
 
