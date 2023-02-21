4295fbf7c453ec0f7ee5966bb36c6ffd9927292b
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 11:22:12 -0800

    number 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,19 +4,17 @@
  *
  *Return: always 0(success)
  */
+void print_alphabet()
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
 int main(void)
 {
-	void print_alphabet()
-	{	char letter;
-
-		for (letter = 'a'; letter <= 'z'; letter++) 
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
-				
-	
-	}
 	print_alphabet();
 	return 0;
 }
