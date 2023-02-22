112d533cf9aa8b8b1e99a4f6bf987efbb5509877
PatrickOjiambo <pashrick237@gmail.com>
2023-02-22 10:29:58 -0800

    number 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int a = 1, b = 1, c = 2;
+    int sum_even = 0;
+
+    while (c <= 4000000)
+    {
+        if (c % 2 == 0)
+        {
+            sum_even += c;
+        }
+        a = b;
+        b = c;
+        c = a + b;
+    }
+
+    printf("%d\n", sum_even);
+    return (0);
+}
+
