d04b234c6a9f8cc9ecb4e527a60b406c4bbd4a1c
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 20:31:40 -0800

    number 5

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,8 +4,8 @@
  *_abs - Prints the absolute value of a number
  *@n: Number to be checked
  *
- *return: Returns -n if n is less than 0 else it returns
- *original value of n
+ *Return: Absolute value of n
+ *
  */
 int _abs(int n)
 {
