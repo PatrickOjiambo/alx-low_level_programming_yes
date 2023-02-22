3e36b2a9217a231f3a2b838f7943d78601c8d2e9
PatrickOjiambo <pashrick237@gmail.com>
2023-02-22 10:33:37 -0800

    number 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-    int a = 1, b = 1, c = 2;
-    int sum_even = 0;
+	int a = 1, b = 1, c = 2;
+	int sum_even = 0;
 
-    while (c <= 4000000)
-    {
-        if (c % 2 == 0)
-        {
-            sum_even += c;
-        }
-        a = b;
-        b = c;
-        c = a + b;
-    }
+	while (c <= 4000000)
+	{
+		if (c % 2 == 0)
+		{
+			sum_even += c;
+		}
+		a = b;
+		b = c;
+		c = a + b;
+	}
 
-    printf("%d\n", sum_even);
-    return (0);
+	printf("%d\n", sum_even);
+	return (0);
 }
 
