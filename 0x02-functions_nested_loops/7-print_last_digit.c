514ce9dad18655e04ec0279beaa52818ecc71f2f
PatrickOjiambo <pashrick237@gmail.com>
2023-02-21 21:06:50 -0800

    num 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,9 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	long int c = n;
-	if (n < 0)
-	{
-		c = -c;
-	}
-	c = n % 10;
+
+	last_digit = _abs(n) % 10;
 	_putchar(last_digit + '0');
-	return last_digit;
+
+	return (last_digit);
 }
