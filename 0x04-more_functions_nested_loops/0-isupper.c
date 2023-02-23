243f5cf32b46affe542cf8406f0616d450b8efd7
PatrickOjiambo <pashrick237@gmail.com>
2023-02-23 06:18:47 -0800

    number 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 /**
- *_isupper() - Checks if a number is uppercase
- *@c- The number to be checked
+ *_isupper - Checks if a number is uppercase
+ *@c:  The number to be checked
  *
  *Return - Returns 1 if c is uppercase, else it returns 0
  */
@@ -10,6 +10,6 @@ int _isupper(int c)
 {
 	if (isupper((c)))
 		return (1);
-	else 
+	else
 		return (0);
 }
