7406bb80e53576442f09cd31d03e3cded4a316f3
PatrickOjiambo <pashrick237@gmail.com>
2023-02-23 05:26:42 -0800

    number 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+/**
+ *print_line - Draws a straight line across the terminal
+ *@n: the value to b checked
+ *Return: void
+ */
+#include "main.h"
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
