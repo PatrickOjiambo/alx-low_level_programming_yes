0e3db60a66f79e080f173aeada0ca7f46bf8e2cb
PatrickOjiambo <pashrick237@gmail.com>
2023-02-27 10:43:36 -0800

    number 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,13 @@
 void rev_string(char *s)
 {
 	int len, i;
+	char temp;
 
 	len = strlen(s);
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i < len / 2; i++)
 	{
-	_putchar(s[i]);
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
 	}
 }
