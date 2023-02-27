816a4dd16f254e221db20dee3f7b254c8bf6a02a
PatrickOjiambo <pashrick237@gmail.com>
2023-02-27 09:15:55 -0800

    number 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *swap_int - Swaps integers
+ *@a: Viable to be swapped
+ *@b: Variable to be swapped
+ *return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	*a = temp;
+	*b = *a;
+	temp = *a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef _PUTCHAR_H
 #define _PUTCHAR_H
-
+void swap_int(int *a, int *b);
 int _putchar(char c);
 void reset_to_98(int *n);
 #endif /* _PUTCHAR_H */
