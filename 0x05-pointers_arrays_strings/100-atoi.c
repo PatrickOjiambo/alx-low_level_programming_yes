f683b8edb9223a703f59a12a8bd8e54f1a340207
PatrickOjiambo <pashrick237@gmail.com>
2023-02-28 09:40:52 -0800

    number 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,39 +8,26 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
 	int result = 0;
+	int sign = 1;
 	int i = 0;
-	int digit = s[i] - '0';
-
-	if (s == NULL)
-	return (0);
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 		{
-			sign = -sign;
-		}
-		else if (s[i] == '+')
-		{
-			sign = 1;
+			sign = -1;
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (result > INT_MAX / 10 ||
-			(result == INT_MAX / 10 && digit > INT_MAX % 10))
-			{
-				return ((sign == -1) ? INT_MIN : INT_MAX);
-			}
-			result = result * 10 + digit;
+			result = result * 10 + (s[i] - '0');
 		}
-		else
+		else if (result > 0)
 		{
 			break;
 		}
 		i++;
 	}
-	return (result * sign);
+	return (sign * result);
 }
 
