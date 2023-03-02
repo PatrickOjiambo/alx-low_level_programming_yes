455c2defcb1cff1641afb23fa040cf6692e84a1c
PatrickOjiambo <pashrick237@gmail.com>
2023-03-02 09:24:26 -0800

    number 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	int i = 0;
+
+	while (s1[i] == s2[i])
 	{
-		*s1++;
-		*s2++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			return (0);
+		}
+		i++;
 	}
-	return (*(const unsigned char *)s1 - * (const unsigned char *)s2);
+	return (s1[i] - s2[i]);
 }
 
