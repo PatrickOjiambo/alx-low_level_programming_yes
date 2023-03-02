5954f9acf9d5381219e111405f2fd1d1950e1f59
PatrickOjiambo <pashrick237@gmail.com>
2023-03-02 09:08:55 -0800

    number 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,11 +6,11 @@
  *@n: Size in bytes
  *Return: always 0-success
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i < 0; i< n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		src[i] = dest[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
