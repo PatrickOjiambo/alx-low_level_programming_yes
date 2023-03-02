b10738b3546051ef79487895014e84015dca8b17
PatrickOjiambo <pashrick237@gmail.com>
2023-03-02 08:48:22 -0800

    number 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stddef.h>
 /**
  *_strncat - Appends two functions together
  *@dest: destination of the file
@@ -6,3 +7,13 @@
  *@n: Max number of bytes
  *Return: a pointer to the resulting string dest
  */
+char *_strncat(char *dest, char *src, int n);
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
