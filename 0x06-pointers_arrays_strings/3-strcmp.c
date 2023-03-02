25afb8b6d4be47c4070ea093edffc04f794b64cd
PatrickOjiambo <pashrick237@gmail.com>
2023-03-02 09:17:14 -0800

    number 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,4 +5,13 @@
  *@s2: Parameter 2
  *Return: always true
  */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		*s1++;
+		*s2++;
+	}
+	return (*(const unsigned char *)s1 - * (const unsigned char *)s2);
+}
 
