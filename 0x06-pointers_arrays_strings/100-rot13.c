dd8be6af57c6a6400ab3cda9935350026a40b41a
PatrickOjiambo <pashrick237@gmail.com>
2023-03-02 12:33:08 -0800

    number 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: String to encode
+ *
+ * Return: Pointer to encoded string
+ */
+char *rot13(char *s)
+{
+	char *p = s;
+	int i, j;
+
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char abc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	while (*p)
+	{
+		for (i = 0; abc[i]; i++)
+		{
+			if (*p == abc[i])
+			{
+				*p = rot13[i];
+				break;
+			}
+		}
+		p++;
+	}
+
+	return (s);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef _PUTCHAR_H
 #define _PUTCHAR_H
+char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
