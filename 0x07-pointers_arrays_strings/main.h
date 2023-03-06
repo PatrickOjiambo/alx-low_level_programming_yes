e88a5b5250bc87608b1a25ca506e59f3cd14c72e
PatrickOjiambo <pashrick237@gmail.com>
2023-03-06 10:17:26 -0800

    number 100

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef _PUTCHAR_H
 #define _PUTCHAR_H
-
+void set_string(char **s, char *to);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
