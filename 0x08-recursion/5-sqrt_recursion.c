175d4cb9a59026f852f5af98800a8524869fea10
PatrickOjiambo <pashrick237@gmail.com>
2023-03-08 08:34:54 -0800

    number 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,15 +6,4 @@
  *or -1 if n does not have a natural square root
  */
 #include "main.h"
-int _sqrt_recursion(int n)
-{
-	int root;
-
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	root = _sqrt_helper(n, 1, n / 2);
-	return (root);
-}
 
