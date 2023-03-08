2d985b70cb1e925cc488fe0593f3d72814ea4a27
PatrickOjiambo <pashrick237@gmail.com>
2023-03-08 08:03:40 -0800

    number 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,10 +6,17 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int half_pow;
+
 	if (y == 0)
 		return (1);
 	else if (y < 0)
 		return (-1);
+	else if (y % 2 == 0)
+	{
+		half_pow = _pow_recursion(x, y / 2);
+		return (half_pow * half_pow);
+	}
 	else if (y > 1)
 		return (x * _pow_recursion(x, y - 1));
 	else
