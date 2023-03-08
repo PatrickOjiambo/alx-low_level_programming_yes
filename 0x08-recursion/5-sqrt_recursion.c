50c7195bedfdeda25ca5134c3a68f2bd033b84c7
PatrickOjiambo <pashrick237@gmail.com>
2023-03-08 08:20:25 -0800

    number 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,26 +18,3 @@ int _sqrt_recursion(int n)
 	return (root);
 }
 
-/**
- * _sqrt_helper - recursive helper function for _sqrt_recursion
- * @n: the number to calculate the square root of
- * @min: the minimum possible square root
- * @max: the maximum possible square root
- *
- * Return: the natural square root of n
- */
-int _sqrt_helper(int n, int min, int max)
-{
-	int guess, square;
-
-	if (max < min)
-		return (-1);
-	guess = (min + max) / 2;
-	square = guess * guess;
-	if (square == n)
-		return (guess);
-	else if (square < n)
-		return (_sqrt_helper(n, guess + 1, max));
-	else
-		return (_sqrt_helper(n, min, guess - 1));
-}
