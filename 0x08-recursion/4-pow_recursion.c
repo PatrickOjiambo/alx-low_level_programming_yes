198a6ac9d09f95c0dc0469a0f5fe45c4ac497ea9
PatrickOjiambo <pashrick237@gmail.com>
2023-03-08 07:50:23 -0800

    number 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ *_pow_recursion - Prints x to the power y
+ *@x: base number
+ *@y: The power number
+ *Return: the value of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y > 1)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (-1);
+}
