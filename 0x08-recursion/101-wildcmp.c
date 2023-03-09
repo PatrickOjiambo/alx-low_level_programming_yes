4c11566ac5c69c04fc141a2978742539ebf0be8a
PatrickOjiambo <pashrick237@gmail.com>
2023-03-08 19:00:17 -0800

    100

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+/**
+ * wildcmp - compare two strings with wildcard support
+ * @s1: the first string to compare
+ * @s2: the second string to compare, which can contain wildcard '*'
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise
+ */
+#include "main.h"
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else
+		{
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		}
+	}
+	else if (*s1 == *s)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else
+		return (0);
+}
+
