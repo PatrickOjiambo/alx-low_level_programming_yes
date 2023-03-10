b89fd2aef8dbc37485615de44b3fc5c433b27b06
PatrickOjiambo <pashrick237@gmail.com>
2023-03-10 11:04:09 -0800

    Number 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,4 +6,22 @@
  *or -1 if n does not have a natural square root
  */
 #include "main.h"
+#include <math.h>
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	for (i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
 
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ *main - Start of the program
+ *@argc: Parameter 1
+ *@argv: Parameter 2
+ *Return: Integer
+ */
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
