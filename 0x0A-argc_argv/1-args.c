b235695a8411af576ebad6eda060718596f85dd4
PatrickOjiambo <pashrick237@gmail.com>
2023-03-10 11:38:58 -0800

    number 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,13 @@
+/**
+ *main - start of the program
+ *@argc: Param 1
+ *@argv: Param 2
+ *Return: Void
+ */
 #include <stdio.h>
-
 int main(int argc, char *argv[])
 {
-    printf("%d\n", argc - 1);
-    return 0;
+	printf("%d\n", argc - 1);
+	return (0);
 }
 
