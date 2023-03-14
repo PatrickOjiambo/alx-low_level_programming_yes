da445c788a9199c0783dab07e5ac3d2cc8ee49e2
PatrickOjiambo <pashrick237@gmail.com>
2023-03-14 07:20:57 -0700

    Number 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+/**
+ *_strdup - Function to be created
+ *@str:only parameter
+ *Return: Return integer
+ */
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+char *_strdup(char *str)
+{
+	char *duplicate = (char *) malloc((strlen(str) + 1) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(duplicate, str);
+	return (duplicate);
+}
+
