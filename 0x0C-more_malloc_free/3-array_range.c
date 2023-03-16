abed4130f9ce61997f8fe509d0c4e4e939f82755
PatrickOjiambo <pashrick237@gmail.com>
2023-03-16 09:58:14 -0700

    Number 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,8 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
-	arr = malloc(sizeof(int) * size);
-	
+	arr = malloc(sizeof(int) * size);	
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++, min++)
