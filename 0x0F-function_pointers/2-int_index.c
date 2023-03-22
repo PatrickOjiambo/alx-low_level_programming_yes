b96d8f5f0a6c01f0041b231c36ec02b878233cb2
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-03-22 11:41:01 -0700

    Number 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
 #include <stddef.h>
-
 /**
  * int_index - searches for an integer
  *
@@ -12,17 +11,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
-
-    if (array == NULL || cmp == NULL || size <= 0)
-        return (-1);
-
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-            return (i);
-    }
-
-    return (-1);
+	int i;
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
 
