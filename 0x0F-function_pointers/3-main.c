ac325e9db8fadad3dce786187a35272422929b11
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-03-22 12:00:45 -0700

    Number 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple arithmetic operations
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0 if successful, 98 if wrong number of arguments, 99 if invalid operator,
+ * or 100 if division by 0
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*op_func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    op_func = get_op_func(argv[2]);
+
+    if (op_func == NULL || argv[2][1] != '\0')
+    {
+        printf("Error\n");
+        return (99);
+    }
+
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
+
+    result = op_func(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
+
