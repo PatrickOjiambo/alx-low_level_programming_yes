339b515d71a7c3acf45323f129a5a7e9bc1011c1
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-04-09 12:28:24 -0700

    Bit manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,9 @@
 #include "main.h"
 int get_endianness(void) {
-  unsigned int value = 0x12345678;
-  unsigned char *ptr = (unsigned char *)&value;
+  unsigned int value;
+  unsigned char *ptr;
+  value = 0x12345678;
+  *ptr = (unsigned char *)&value;
   if (*ptr == 0x78) {
     return 1; // little endian
   } else {
