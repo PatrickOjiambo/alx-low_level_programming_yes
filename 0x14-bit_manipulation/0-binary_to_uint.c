b934930ef41f8f340aff75f44888e5e050ef8fc8
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-04-09 12:11:30 -0700

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,20 @@
-#include <stddef.h>  // for NULL
-#include <stdint.h>  // for uint32_t
-#include <string.h>  // for strlen
+#include <stddef.h>  
+#include <stdint.h>  
+#include <string.h> 
 #include "main.h"
 uint32_t binary_to_uint(const char *b) {
+
+	size_t len;
+	uint32_t result;
+	size_t i;
   if (b == NULL) {
     return 0;
   }
 
-  size_t len = strlen(b);
-  uint32_t result = 0;
 
-  for (size_t i = 0; i < len; i++) {
+  len = strlen(b);
+  result = 0;
+  for (i = 0; i < len; i++) {
     if (b[i] == '0') {
       result = (result << 1);
     } else if (b[i] == '1') {
